454cb81e8b9fd994c8d19d12c679fb77b24e036b
MiralBhalani <ranakamal7398@gmail.com>
2023-06-10 20:50:41 +0530

    Added TODO points and task.txt file

--- hello_world.js
+++ hello_world.js
@@ -1,5 +1,5 @@
 // 'require' is use for load module
-var http = require('http');
+var http = require('http'); //TODO do not use var use const or let please try to implement latest JS
 
 // load custom module
 var customModule = require('./custom_modules/first_module');
@@ -8,15 +8,15 @@ var customModule = require('./custom_modules/first_module');
 var url = require('url');
 
 // file server module for doing operations on file
-var fs = require('fs'); 
+var fs = require('fs');
 
 // Create object for server
 http.createServer(function (req, res) {
     // response header for meta data
-    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.writeHead(200, { 'Content-Type': 'text/html' });
 
     // load custom module method
-    res.write("Load from custom moduel = "+customModule.myDateTime());
+    res.write("Load from custom moduel = " + customModule.myDateTime());
 
     // write method is use to stream the data
     res.write('Hello World ....');
@@ -26,21 +26,35 @@ http.createServer(function (req, res) {
 
     // for parse the query param
     var q = url.parse(req.url, true).query;
-
     // store the query param data
     var name = q.name || '-';
     res.write(name);
 
     // For read the file
-    fs.readFile('files/sample.txt', function(err, data) {
-        res.write(data);
-        return res.end();
-    });
+    const fileName = './files/sample.txt';
+    fs.readFile(fileName, "utf8", function (err, data) {//TODO if your file name is same so store in variable const and use both place to reduce readablity
+        if (err) {
+            throw new Error(err);
+        }
+        // else {
+        //     //TODO ths is never work because this fs fuction is sync behaviour so your data is never write in brower page.
+        //     res.write(data);
+        //     res.end();
+        // }
+    })
 
     //For append the file
-    fs.appendFile('./files/sample.txt', '\nYes I am fine', function (err) {
-        if (err) throw err;
-        res.write('Saved!');
-        return res.end();
+    fs.appendFile(fileName, '\nYes I am fine', function (err) {
+        if (err) {
+            throw new Error(err);
+        }
+        // TODO No need her also to wait because write steam never wait for end your runing code going in next line so That error you get in fs function
     });
-}).listen(8080);
+    res.end('Saved!'); // here need to add end()
+
+}).listen(8080, function () { //TODO write like this coz your server is started so its show in CMD
+    console.log("server running on port 8080");
+});
+
+// You get this error because you write and end res 'ERR_STREAM_WRITE_AFTER_END'
+// you get this error because you do not handle your error in this project
--- url_module.js
+++ url_module.js
@@ -1,12 +1,15 @@
 var url = require('url');
 var http = require('http');
-var uc = require('upper-case'); // NPM Package
+// var uc = require('upper-case'); // NPM Package //TODO why you install this instead of use javascript function ex: toUpperCase();
 
-http.createServer(function(req, res){
-    res.writeHead(200, {'Content-Type': 'text/htl'});
-    var adr = 'http://localhost:8080/default.htm?year=2023&month=may';
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' }); //TODO text/html please make sure verify your code before push
+
+    var adr = 'http://localhost:8080/default.htm?year=2023&month=may'; //TODO do not use static way try to use dynamic
+
+
     var q = url.parse(adr, true);
-    
+
     var queryYear = q.query.year; // q.query return object with query param data
     var host = q.host;
     var pathname = q.pathname;
@@ -16,6 +19,7 @@ http.createServer(function(req, res){
     res.write(pathname);
     res.write(search);
     res.write(queryYear);
-    res.end(uc.upperCase('done'));
+    const str = 'Done';
+    res.end(str.toUpperCase());
 
 }).listen(8080);
